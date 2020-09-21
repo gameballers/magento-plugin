c189b4e4d287df10dfa72f5d792bc7129483272c
Hana <1999hanaamr@gmail.com>
2020-09-21 03:13:22 +0200

    Removing cacheable=false

--- Block/LoadAPIKey.php
+++ Block/LoadAPIKey.php
@@ -10,25 +10,17 @@ class LoadAPIKey extends \Magento\Framework\View\Element\Template
     protected $clientKeys;
 
     /**
-     * @var $customerSession
-     */
-    protected $customerSession;
-
-    /**
      * LoadAPIKey constructor.
      *
      * @param \Magento\Framework\View\Element\Template\Context      $context
-     * @param \Magento\Customer\Model\Session                       $customerSession
      * @param \GbPlugin\Integration\Observer\Shared\ClientkeysTable $clientKeys
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
-        \Magento\Customer\Model\Session $customerSession,
         \GbPlugin\Integration\Observer\Shared\ClientkeysTable $clientKeys
     ) {
         parent::__construct($context);
         $this->clientKeys = $clientKeys;
-        $this->customerSession = $customerSession;
     }
     /**
      * Method to return apikey
@@ -46,7 +38,9 @@ class LoadAPIKey extends \Magento\Framework\View\Element\Template
      */
     public function getCustomerSession()
     {
-        return $this->customerSession;
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $customerSession = $objectManager->create('Magento\Customer\Model\Session');
+        return $customerSession;
     }
 
 }
